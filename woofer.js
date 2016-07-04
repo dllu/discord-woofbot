5cdb0569e7a7cec050bdc68e217de0e18d01ba74
Daniel Lu <daniel@lawrence.lu>
2016-07-03 20:36:17 -0400

    allow you to skip turns, strip leading and trailing whitespace

--- woofer.js
+++ woofer.js
@@ -11,15 +11,19 @@ var chessmsg = {};
 var thinking = {};
 
 var CHESS = 'puppy chess';
-var DEPTH = 3;
+var MOVETIME = 300;
 
 var SIDENAMES = {w:'Black', b:'White'};
+
+function strip(s) {
+    return s.replace(/^\s+|\s+$/g, '');
+}
 
 function end_game(id, resign) {
     if(chesses[id] === undefined) return;
     var winner;
     if(resign) {
-        winner = 'Black wins by resignation!';
+        winner = SIDENAMES[chesses[id].turn()] + ' wins by resignation!';
     } else if(chesses[id].in_checkmate()) {
         winner = SIDENAMES[chesses[id].turn()] + ' wins by checkmate!';
     } else if(chesses[id].in_stalemate()) {
@@ -31,44 +35,65 @@ function end_game(id, resign) {
     } else if(chesses[id].in_draw()) {
         winner = 'Draw!';
     }
-    puppy.reply(chessmsg[id], 'http://i.imgur.com/w4TR9IT.png Game over: ' + winner);
+    puppy.reply(chessmsg[id], 'http://i.imgur.com/w4TR9IT.png Game over: ' + winner + 
+            '\n' + chesses[id].pgn({newline_char: '\n'}));
     delete chesses[id];
     delete stockfishes[id];
     delete thinking[id];
 }
 
 function get_fen_img(id) {
-    return 'http://www.fen-to-image.com/image/' + chesses[id].fen().split(' ')[0];
+    return 'http://www.fen-to-image.com/image/20/single/coords/' + chesses[id].fen().split(' ')[0];
+/*
+ * apparently discord font is not truly monospace so chess characters are wider
+    var s = chesses[id].ascii();
+    var chess_characters = {
+        r: '♜', n: '♞', b: '♝', q: '♛', k: '♚', p: '♟', R: '♖', N: '♘', B: '♗', Q: '♕', K: '♔', P: '♙'
+        };
+    for(var c in chess_characters) {
+        var re = new RegExp(c, 'g');
+        s = s.replace(re, chess_characters[c]);
+    }
+    s = s.replace(/^/g,'`');
+    s = s.replace(/$/g,'`');
+    return s;
+*/
 }
 
 
 puppy.on('message', function(message) {
-    if(message.content === 'woof') {
+    var msg = strip(message.content);
+    if(message.isMentioned(puppy.user)) {
+        if(msg.indexOf('help') !== -1) {
+            puppy.reply(message, 'Valid commands are: `woof`, `bark`, `sleep tight pupper`, `puppy chess [algebraic chess notation]`, `puppy chess resign`, `puppy why`');
+        }
+    }
+
+    if(msg === 'woof') {
         var s = 'woof';
         if(Math.random() < 0.01) {
             s += ' http://i.imgur.com/49ZT1dG.png';
         }
         puppy.reply(message, s);
-    } else if(message.content === 'bark') {
+    } else if(msg === 'bark') {
         puppy.reply(message, 'bark');
-    } else if(message.content === 'sleep tight pupper') {
+    } else if(msg === 'sleep tight pupper') {
         puppy.reply(message, 'http://i.imgur.com/ZbkyEk8.png');
-    } else if(message.content === 'puppy why') {
+    } else if(msg === 'puppy why') {
         puppy.reply(message, why.why());
-    } else if(message.content.indexOf(CHESS) === 0) {
+    } else if(msg.indexOf(CHESS) === 0) {
         var id = message.author.id + '!?#' + message.server.id;
         chessmsg[id] = message;
         if(chesses[id] === undefined) {
             chesses[id] = new Chess();
             thinking[id] = false;
             stockfishes[id] = Stockfish();
-            stockfishes[id].postMessage('setoption name Contempt value 1');
-            stockfishes[id].postMessage('setoption name King Safety value 1');
+            stockfishes[id].postMessage('setoption name Contempt value 30');
             stockfishes[id].postMessage('setoption name Skill Level value 20');
             stockfishes[id].postMessage('ucinewgame');
             stockfishes[id].postMessage('isready');
             stockfishes[id].onmessage = function(event) {
-                console.log(event);
+                //console.log(event);
                 var line;
                 if(event && typeof event === 'object') {
                     line = event.data;
@@ -78,7 +103,7 @@ puppy.on('message', function(message) {
                 var match = line.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbn])?/);
                 if(match) {
                     var m = chesses[id].move({from: match[1], to: match[2], promotion: match[3]});
-                    puppy.reply(chessmsg[id], get_fen_img(id) + '  ' + m.san);
+                    puppy.reply(chessmsg[id], m.san + '\n' + get_fen_img(id));
                     thinking[id] = false;
                     if(chesses[id].game_over()) {
                         end_game(id, false);
@@ -86,7 +111,7 @@ puppy.on('message', function(message) {
                 }
             }
         }
-        var move = message.content.substring(CHESS.length + 1);
+        var move = strip(msg.substring(CHESS.length + 1));
         if(move === 'resign') {
             end_game(id, true);
             return;
@@ -95,13 +120,14 @@ puppy.on('message', function(message) {
             puppy.reply(chessmsg[id], "I'm still thinking...");
             return;
         }
-        if(chesses[id].move(move, {sloppy: true}) === null) {
-            puppy.reply(chessmsg[id], 'Woof! Illegal move!');
+        if(move !== 'skip' && chesses[id].move(move, {sloppy: true}) === null) {
+            puppy.reply(chessmsg[id], 'Woof! Illegal move! Valid moves are: ' + chesses[id].moves().join(', ') + 
+                    '\n' + get_fen_img(id));
             return;
         }
         thinking[id] = true;
         stockfishes[id].postMessage('position fen ' + chesses[id].fen());
-        stockfishes[id].postMessage('go depth ' + DEPTH);
+        stockfishes[id].postMessage('go movetime ' + MOVETIME);
         if(chesses[id].game_over()) {
             end_game(id, false);
         }
